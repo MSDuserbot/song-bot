a4a03995aa39c4a4faeb966f9abfee7861816484
MSDkingdom <84111953+MSDuserbot@users.noreply.github.com>
2022-07-18 09:15:30 +0530

    Update song.py

--- modules/song.py
+++ modules/song.py
@@ -88,7 +88,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎶 <b>ᴛɪᴛʟᴇ:</b> <a href="{link}">{title}</a>\n⌚ <b>ᴅᴜʀᴀᴛɪᴏɴ:</b> <code>{duration}</code>\n📻 <b>:</b> <a hrᴜᴘʟᴏᴀᴅᴇᴅ ʙʏef="https://t.me/RheaXBot">ʀʜᴇᴀ sᴏɴɢ ʙᴏᴛ</a>\n👻<b>Sᴜᴘᴘᴏʀᴛ:</b> <a href="https:t.me/OlympusChatting">Oʟʏᴍᴘᴜs Cʜᴀᴛᴛɪɴɢ</a>'
+        rep = f'🎶 <b>ᴛɪᴛʟᴇ:</b> <a href="{link}">{title}</a>\n⌚ <b>ᴅᴜʀᴀᴛɪᴏɴ:</b> <code>{duration}</code>\n📻 <b>ᴜᴘʟᴏᴀᴅᴇᴅ ʙʏ:</b> <a href="https://t.me/RheaXBot">ʀʜᴇᴀ sᴏɴɢ ʙᴏᴛ</a>\n👻 <b>Sᴜᴘᴘᴏʀᴛ:</b> <a href="https:t.me/OlympusChatting">Oʟʏᴍᴘᴜs Cʜᴀᴛᴛɪɴɢ</a>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
